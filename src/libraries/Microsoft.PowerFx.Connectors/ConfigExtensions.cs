f5daa32943140a44d44b1d91882a373054b92bfe
Mike Stall <jmstall@microsoft.com>
2022-05-18 11:33:41 -0700

    Can call real connector.

--- src/libraries/Microsoft.PowerFx.Connectors/ConfigExtensions.cs
+++ src/libraries/Microsoft.PowerFx.Connectors/ConfigExtensions.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
+using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 using Microsoft.OpenApi.Readers;
 using Microsoft.PowerFx.Connectors;
@@ -29,6 +30,21 @@ namespace Microsoft.PowerFx
         // $$$ Return the functions that we did add?
         public static void AddService(this PowerFxConfig config, string @namespace, OpenApiDocument openApiDocument, HttpClient httpClient)
         {
+            if (openApiDocument == null)
+            {
+                throw new ArgumentNullException(nameof(openApiDocument));
+            }
+
+            string basePath = null;
+            if (openApiDocument?.Servers.Count == 1)
+            {
+                // This is a full URL that will pull in 'basePath' property from connectors. 
+                // Extract BasePath back out from this. 
+                var fullPath = openApiDocument.Servers[0].Url;
+                var uri = new Uri(fullPath);
+                basePath = uri.PathAndQuery;
+            }
+
             // $$$ Use namespace. 
             var model = new OpenApiModel(openApiDocument);
 
@@ -44,17 +60,45 @@ namespace Microsoft.PowerFx
 
                     // $$$ Pull description/summary for intellisense?
                     var operationName = op.OperationId ?? path.Replace("/", string.Empty);
+
                     var paramTypes = new List<FormulaType>();
+                    var requiredParams = new List<OpenApiParameter>();
 
                     foreach (var param in op.Parameters)
                     {
+                        var exts = param.Extensions;
+                        if (exts.TryGetValue("x-ms-visibility", out var val))
+                        {
+                            if (val is OpenApiString valStr)
+                            {
+                                if (valStr.Value == "internal")
+                                {
+                                    // connectionId is a well-known internal param, stamped later. 
+                                    if (param.Name != "connectionId")
+                                    {
+                                        throw new NotImplementedException();
+                                    }
+
+                                    continue;
+                                }
+                            }
+                        }
+
                         var paramType = model.ToType(param.Schema);
                         paramTypes.Add(paramType);
+                        requiredParams.Add(param);
                     }
 
                     var returnType = model.GetReturnType(op);
+                    
+                    if (basePath != null)
+                    {
+                        path = basePath + path;
+                    }
 
-                    var invoker = new Invoker(httpClient, verb, path, op.Parameters.ToArray());
+                    var invoker = (httpClient == null) ? 
+                        null : 
+                        new Invoker(httpClient, verb, path, requiredParams.ToArray());
 
                     var function = new MyTexlFunction(operationName, returnType, paramTypes.ToArray())
                     {
@@ -87,8 +131,8 @@ namespace Microsoft.PowerFx
             {
                 var path = _path;
 
-                var connectionId = "xyz";
-                path = path.Replace("{connectionId}", connectionId);
+                // var connectionId = "xyz";
+                // path = path.Replace("{connectionId}", connectionId);
                 var query = new StringBuilder();
 
                 for (var i = 0; i < _parameters.Length; i++)
@@ -130,6 +174,9 @@ namespace Microsoft.PowerFx
                     var msg = $"Connector called failed {response.StatusCode}): " + json;
 
                     // $$$ Do any connectors have 40x behavior here in their response code?
+                    // or 201 long-ops behavior?
+
+                    // $$$ Still type this. 
                     return FormulaValue.NewError(new ExpressionError
                     {
                         Kind = ErrorKind.Unknown, 
@@ -145,7 +192,7 @@ namespace Microsoft.PowerFx
             public async Task<FormulaValue> InvokeAsync(CancellationToken cancel, FormulaValue[] args)
             {
                 var request = BuildRequest(args);
-                var response = await _httpClient.SendAsync(request);
+                var response = await _httpClient.SendAsync(request, cancel);
 
                 var result = await DecodeResponseAsync(response);
                 return result;
---
+++ src/libraries/Microsoft.PowerFx.Connectors/PowerPlatformConnectorClient.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.PowerFx.Connectors
+{
+    /// <summary>
+    /// Http handler to invoke Power Platform connectors. 
+    /// </summary>
+    public class PowerPlatformConnectorClient : HttpClient
+    {
+        private readonly HttpClient _client = new HttpClient(); // $$$ Share
+
+        public string ConnectionId;
+
+        public PowerPlatformConnectorClient(string endpoint)
+        {
+            Endpoint = endpoint;
+            _client.BaseAddress = new Uri("https://" + endpoint);
+
+            BaseAddress = _client.BaseAddress; // Needed for callers. 
+        }
+
+        public string Endpoint;
+
+        // Stamp on request.  Need to refresh?
+        public string AuthToken;
+
+        public string EnvironmentId;
+
+        public override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var url = request.RequestUri.ToString();
+            if (url[0] != '/')
+            {
+                // Client has Basepath set. 
+                // x-ms-request-url needs relative URL. 
+                throw new InvalidOperationException($"URL should be relative for x-ms-request-url property");
+            }
+
+            url = url.Replace("{connectionId}", ConnectionId);
+
+            var method = request.Method;
+            var useCache = method == HttpMethod.Get;
+            
+            var req = new HttpRequestMessage(HttpMethod.Post, $"https://{Endpoint}/invoke");
+            req.Headers.Add("authority", Endpoint);
+            req.Headers.Add("scheme", "https");
+            req.Headers.Add("path", "/invoke");
+            req.Headers.Add("x-ms-client-session-id", "f4d37a97-f1c7-4c8c-80a6-f300c651568d");
+            req.Headers.Add("x-ms-request-method", method.ToString());
+            req.Headers.Add("authorization", "Bearer " + AuthToken);
+            req.Headers.Add("x-ms-client-environment-id", "/providers/Microsoft.PowerApps/environments/" + EnvironmentId);
+            req.Headers.Add("x-ms-user-agent", "PowerApps/3.21124.0 (Web AuthoringTool; AppName=<NonCloudApp>)");
+
+            req.Headers.Add("x-ms-request-url", url); 
+
+            // $$$ Body? 
+
+            // $$$ Need to sue basePath
+
+            var response = await _client.SendAsync(req, cancellationToken);
+            return response;
+        }
+    }
+}
